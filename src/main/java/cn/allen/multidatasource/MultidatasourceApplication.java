320e90ea8c53d811a1ace0c67b686d607acde413
allen <qingjie.du@iquantex.com>
2020-04-20 21:12:33 +0800

    git init

---
+++ src/main/java/cn/allen/multidatasource/MultidatasourceApplication.java
@@ -0,0 +1,13 @@
+package cn.allen.multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultidatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultidatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/allen/multidatasource/MultidatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.allen.multidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
